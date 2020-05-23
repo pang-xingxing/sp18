477b8a8a5dbfa4168d3b451d7eb2f5245a02a859
Kan Yuxin <njmsk@DESKTOP-I0B7DUS.mit.edu>
2020-05-23 16:42:37 +0800

    completed first part of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,17 +1,26 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Yuxin Kan
  */
 public class LeapYear {
 
-    /** Calls isLeapYear to print correct statement.
-     *  @param  year to be analyzed
-     */
+    /** A leap year is either:divisible by 400 or divisible by 4 and not by 100.*/
+
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
         } else {
             System.out.printf("%d is not a leap year.\n", year);
         }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0){
+            return true;
+        }
+        if (year % 4 ==0 && year % 100 != 0){
+            return true;
+        }
+        return false;
     }
 
     /** Must be provided an integer as a command line argument ARGS. */
