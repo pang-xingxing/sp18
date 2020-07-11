cc4740ffed36d58390dc68a0b8b35247bce5dc98
Kan Yuxin <njmsk@DESKTOP-I0B7DUS.mit.edu>
2020-07-11 19:13:12 +0800

    finish hw1

---
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -0,0 +1,27 @@
+package synthesizer;
+
+public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
+    protected int fillCount;
+    protected int capacity;
+
+    @Override
+    public int capacity() {
+        return this.capacity;
+    }
+
+    @Override
+    public int fillCount() {
+        return this.fillCount;
+    }
+    /* public boolean isEmpty() {
+        return fillCount == 0;
+    }
+    public boolean isFull() {
+        return fillCount == capacity;
+    }*/
+    public abstract T peek();
+    public abstract T dequeue();
+    public abstract void enqueue(T x);
+
+
+}
